c83f06337d05f149cc6d6fbff40e30f99463cc4c
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-09 14:17:03 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,18 @@
+// for (let i = 0; i  < array.length; i) {
+//     const element = array[i];
+//     console.log();
+    
+// }
+// let a = +prompt("ixtiotiy soni kiriting");
+// let Myfunction = () => {
+// console.log(Math.sqrt(a));
+// }
+// Myfunction()
+function max(a,b) {
+    let c;
+    c = Math.max(a,b);
+    alert("Kiritilgan soning kattasi  = " + c);
+}
+let a = Number (prompt("1 sonni kiriting"));
+let b = Number (prompt("2 sonni kiriting"));
+max(a,b);
